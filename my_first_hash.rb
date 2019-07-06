8a45e7c2fc7fe7d4038b4af281a25f3c7f62d798
Hanna Bergland <berglandhanna@gmail.com>
2019-07-06 18:27:29 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,45 @@
 def my_hash
+  the_hash= {"name"=>"Anna"
+  }
+end  
+
+def shipping_manifest
+  the_manifest= {"whale bone corsets"=>5, "porcelain vases"=>2, "oil paintings"=>3
+    
+  }
+end  
+
+def retrieval
+  the_manifest= {"whale bone corsets"=>5, "porcelain vases"=>2, "oil paintings"=>3
+  }
+  the_manifest["oil paintings"]
+end  
+#call on the key to return the value, not the other way around
+
+def adding
+  the_manifest= {"whale bone corsets"=>5, "porcelain vases"=>2, "oil paintings"=>3
+  }
+  the_manifest["muskets"]=2 
+  the_manifest["gun powder"]=4
+  the_manifest
+end  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
+def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
@@ -41,3 +82,4 @@ def adding
   # return the shipping_manifest hash below
 
 end
+=end
